cf0c7f2597c234b9088d5565240dce687e1d8cb9
Josef Smith <josef.smith@trade.gov.uk>
2023-09-26 16:34:44 +0100

    feat: fetch and run python code from redis

--- dynamic_runner.py
+++ dynamic_runner.py
@@ -1,4 +1,42 @@
+import json
 import sys
 
-print(sys.argv)
+import subprocess
 
+import base64
+
+import pickle
+import redis
+
+
+def hello_world():
+    print("hello world")
+
+def runner(packed_function):
+    import os
+    print("VCAP_SERVICES" in os.environ)
+    unpacked_function = pickle.loads(packed_function)
+    unpacked_function()
+
+if __name__ == '__main__':
+    if sys.argv[1] == "run":
+        import os
+        credentials = json.loads(os.environ["VCAP_SERVICES"])["redis"][0]["credentials"]
+
+        r = redis.Redis(
+            host=credentials["host"], port=credentials["port"],
+            username="",
+            password=credentials["password"],
+            ssl=True,
+        )
+        r.set('key', pickle.dumps(hello_world))
+
+        key = sys.argv[2]
+
+        packed_function = r.get(key)
+        p = subprocess.Popen([sys.executable, __file__, "_run", base64.b64encode(packed_function)], env={"LD_LIBRARY_PATH": os.environ["LD_LIBRARY_PATH"]})
+        p.wait(timeout=30)
+        print(key)
+    
+    if sys.argv[1] == "_run":
+        runner(base64.b64decode(sys.argv[2]))
